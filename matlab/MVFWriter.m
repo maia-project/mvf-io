7b94279f5fa2cef32297cb6f0bc6b1724f7e02f1
Standa Vitek <standa.vitek@gmail.com>
2014-06-07 16:32:15 +0200

    Added templates to MVFWriter, help improved

--- matlab/MVFWriter.m
+++ matlab/MVFWriter.m
@@ -3,8 +3,25 @@ classdef MVFWriter<handle
     %   Use the MVFWriter function with the write method to write MAIA video 
     %   data into a file from the MATLAB workspace.
     % 
-    %   obj = MVFWriter (filename) constructs obj to create new MAIA video object.
+    %   Construction:
+    %
+    %       obj = MVFWriter (filename) construct new MVF video object with
+    %       empty header, header has to be initializaded before writting
+    %       data
+    %
+    %       obj = MVFWriter (filename, 'template', 'standard') construct
+    %       new MVF video object with standard header used in MAIA project
+    %
+    %       obj = MVFWriter (filename, 'template', MVFReader) construct new
+    %       MVF video object with header copied from existing MVFReader
+    %       object
     %   
+    %   Methods:
+    %
+    %       open    open file for writing video data
+    %       write   write one frame to the file
+    %       close   close file after write video data
+    %
     %   Examples of using class:
     %
     %       obj = MVFWriter ('filename.mvf');
@@ -39,9 +56,30 @@ classdef MVFWriter<handle
     
     methods
     
-        function obj = MVFWriter (filename)
+        function obj = MVFWriter (filename, varargin)
             % class constructor
-            obj.filename = filename;
+            if nargin == 1
+                obj.filename = filename;
+            elseif (nargin == 3) && (strcmp(varargin{1}, 'template'))
+                % standard MAIA template
+                if strcmp(varargin{2}, 'standard')
+                    obj.width = 776;
+                    obj.height = 582;
+                    obj.bpp = 16;
+                    obj.bppUsed = 10;
+                    obj.channels = 1;
+                    obj.reserved = 0;
+                    obj.fps = 61;
+                    obj.numOfFrames = 0;
+                end
+                
+                if strcmp (class(varargin{2}), 'MVFReader')
+                    fns = properties(varargin{2});
+                    for i=1:length(fns)
+                        obj.(fns{i}) = varargin{2}.(fns{i});
+                    end
+                end
+            end
         end
         
         function obj = open (obj)
@@ -75,4 +113,3 @@ classdef MVFWriter<handle
     end
     
 end
-
